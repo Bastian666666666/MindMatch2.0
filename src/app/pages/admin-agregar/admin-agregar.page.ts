98c5cb4669d0e0c2c3e968db166a1b3188332247
Bastián Rabelo <ba.rabelo@duocuc.cl>
2024-06-21 03:02:50 -0400

    completo sql

--- src/app/pages/admin-agregar/admin-agregar.page.ts
+++ src/app/pages/admin-agregar/admin-agregar.page.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DbserviceService } from 'src/app/services/dbservice.service';
 
 @Component({
-  selector: 'app-admin-agregar',
-  templateUrl: './admin-agregar.page.html',
-  styleUrls: ['./admin-agregar.page.scss'],
+  selector: 'app-agregar',
+  templateUrl: './agregar.page.html',
+  styleUrls: ['./agregar.page.scss'],
 })
+
 export class AdminAgregarPage implements OnInit {
 
-  constructor() { }
+  usuario = "";
+  contrasena = "";
+  nombre = "";
+  apellido = "";
+  nacimiento = 0;
+
+  constructor(private dbservice: DbserviceService, private router: Router) { }
+
+  guardar() {
+    this.dbservice.addUsuario(this.usuario,this.contrasena,this.nombre,this.apellido,this.nacimiento);
+    this.dbservice.presentToast("Usuario Agregado");
+    this.router.navigate(['/adminhome']);
+  }
 
   ngOnInit() {
   }
