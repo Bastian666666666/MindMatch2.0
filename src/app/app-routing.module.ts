e0e33671bdad4af2150b11b68e14aad02d4bd00f
Bastián Rabelo <ba.rabelo@duocuc.cl>
2024-06-20 21:09:11 -0400

    Agregado hasta conectar android

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+
   {
     path: 'register',
     loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
@@ -29,7 +30,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
 
